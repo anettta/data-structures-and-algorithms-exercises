18b83101f625a13452596bcf5a3dcf8b0fea7b7f
Anya <annagapyuk@gmail.com>
2021-10-21 15:07:07 -0400

    added uncompress

---
+++ 003-uncompress.js
@@ -0,0 +1,37 @@
+// Write a function `uncompress` that takes
+// in a string as an argument.
+// The input string will be formatted
+// into multiple groups according to the following pattern:
+// <number><char>
+// `2c` or `3a`
+// The function should return an uncompressed version of the string
+// where each `char` of a group is
+// repeated `number` times consecutively.
+// You may assume that the input string is well-formed
+// according to the previously mentioned pattern.
+
+// Two-pointer technique!
+
+const uncompress = (str) => {
+  let numbers = "0123456789";
+  let uncompressedString = [];
+  let i = 0;
+  let j = 0;
+
+  while (j < str.length) {
+    if (numbers.includes(str[j])) {
+      j++;
+    } else {
+      let num = Number(str.slice(i, j));
+      for (let count = 0; count < num; count++) {
+        uncompressedString.push(str[j]);
+      }
+      j++;
+      i = j;
+    }
+  }
+
+  return uncompressedString.join("");
+};
+
+console.log(uncompress("2c3a1t")); // 'ccaaat
