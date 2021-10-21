710694df6f88a78f53c31ee5058005c6b0839647
Anya <annagapyuk@gmail.com>
2021-10-21 15:46:48 -0400

    updated compress

--- 004-compress.js
+++ 004-compress.js
@@ -5,11 +5,16 @@
 // Single character occurences should not be changed.
 // 'aaa' compresses to '3a'
 
+// Time: O(n)
+// Space: O(n)
+// Strings in JS are immutable thus strings are copied over
+// using array instead
 const compress = (str) => {
   let compressed = [];
   let i = 0;
   let j = 0;
   while (j <= str.length) {
+    // O(n)
     if (str[j] === str[i]) {
       j++;
     } else {
@@ -17,12 +22,12 @@ const compress = (str) => {
       if (num === 1) {
         compressed.push(str[i]);
       } else {
-        compressed.push(num + str[i]);
+        compressed.push(num, str[i]);
       }
       i = j;
     }
   }
-  return compressed.join("");
+  return compressed.join(""); // O(n)
 };
 
 console.log(compress("ccaaatsss")); // '2c3a1t3s'
