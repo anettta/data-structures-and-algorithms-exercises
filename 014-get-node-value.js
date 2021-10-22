7c0cee0602716ca2d8e1b1508bcb5d6684e77e40
Anya <annagapyuk@gmail.com>
2021-10-22 11:50:35 -0400

    added get-node-value

---
+++ 014-get-node-value.js
@@ -0,0 +1,47 @@
+// Write a function 'getNodeValue' that takes in the head of a linked list and an index.
+// The function should return the value of the linked list at the specified index.
+//
+// If there is no node at the given index, then return null.
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+// iterative
+//
+// const getNodeValue = (head, index) => {
+//   let current = head;
+//   let count = 0;
+//   while (current !== null) {
+//     if (count === index) {
+//       return current.val;
+//     }
+//     current = current.next;
+//     count++;
+//   }
+// };
+//
+// recursive
+//
+const getNodeValue = (head, index) => {
+  if (head === null) {
+    return null;
+  }
+  if (index === 0) {
+    return head.val;
+  }
+  return getNodeValue(head.next, index - 1);
+};
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+console.log(getNodeValue(a, 2)); // 'c'
