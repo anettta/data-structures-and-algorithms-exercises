66f52eef3228702b145631cf201691d8f6a05c0c
Anya <annagapyuk@gmail.com>
2021-10-23 14:10:17 -0400

    updated zipper lists

--- 016-zipper-lists.js
+++ 016-zipper-lists.js
@@ -57,7 +57,7 @@ const zipperLists = (head1, head2) => {
   const next1 = head1.next;
   const next2 = head2.next;
   head1.next = head2;
-  head2.next = zipperLists(next1, next2);
+  head2.next = zipperLists(next1, next2); // head of the remaining list, we chain it to head2.next
   return head1;
 };
 
