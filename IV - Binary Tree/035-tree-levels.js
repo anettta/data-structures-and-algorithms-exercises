104e28f0351b78ead49b42bb600b0864534ab2e1
Anya <annagapyuk@gmail.com>
2021-10-29 19:18:48 -0400

    added tree-levels

---
+++ IV - Binary Tree/035-tree-levels.js
@@ -0,0 +1,61 @@
+// Write a function, treeLevels,
+// that takes in the root of a binary tree.
+// The function should return a 2-Dimensional array
+//  where each subarray represents a level of the tree.
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+// BFS - queue - shift()
+const treeLevels = (root) => {
+  if (root === null) return [];
+
+  let queue = [{ node: root, levelNum: 0 }];
+
+  let levels = [];
+
+  while (queue.length > 0) {
+    const { node, levelNum } = queue.shift();
+
+    if (levels.length === levelNum) {
+      levels.push([node.val]); // for root node
+    } else {
+      levels[levelNum].push(node.val); // indexing into existing subarray
+    }
+
+    if (node.left !== null)
+      queue.push({ node: node.left, levelNum: levelNum + 1 });
+    if (node.right !== null)
+      queue.push({ node: node.right, levelNum: levelNum + 1 });
+  }
+  return levels;
+};
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+
+console.log(treeLevels(a)); // ->
+// [
+//   ['a'],
+//   ['b', 'c'],
+//   ['d', 'e', 'f']
+// ]
