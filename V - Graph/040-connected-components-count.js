85c9816b5ac969e2adadc03abf850ba63c81ed7d
Anya <annagapyuk@gmail.com>
2021-10-30 23:45:30 -0400

    added connected-components-count

--- V - Graph/040-connected-components-count.js
+++ V - Graph/040-connected-components-count.js
@@ -2,6 +2,26 @@
 // that takes in the adjacency list of an undirected graph.
 // The function should return the number of connected components within the graph.
 
+const connectedComponentsCount = (graph) => {
+  const visited = new Set();
+  let count = 0;
+  for (let node in graph) {
+    if (explore(graph, node, visited) === true) {
+      count++;
+    }
+  }
+  return count;
+};
+
+const explore = (graph, current, visited) => {
+  if (visited.has(String(current))) return false;
+  visited.add(String(current));
+  for (let neighbor of graph[current]) {
+    explore(graph, neighbor, visited);
+  }
+  return true;
+};
+
 console.log(
   connectedComponentsCount({
     0: [8, 1, 5],
