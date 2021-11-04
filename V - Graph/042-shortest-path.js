069789dddda1d59c618336b41fd72a67a89f67da
Anya <annagapyuk@gmail.com>
2021-11-04 12:05:36 -0400

    added shortest-path

--- V - Graph/042-shortest-path.js
+++ V - Graph/042-shortest-path.js
@@ -10,13 +10,30 @@
 const shortestPath = (edges, nodeA, nodeB) => {
   const graph = buildGraph(edges);
   // breadth first traversal is better
+  // explore all directions very evenly
+  // queue
+  let visited = new Set([nodeA]);
+  let queue = [[nodeA, 0]];
+  while (queue.length > 0) {
+    const [node, distance] = queue.shift();
+    if (node === nodeB) return distance;
+    // graph[node] is an individual array within adjacency list
+    for (let neighbor of graph[node]) {
+      if (!visited.has(neighbor)) {
+        visited.add(neighbor);
+        queue.push(neighbor, distance + 1);
+      }
+    }
+  }
+  return -1;
 };
 
 const buildGraph = (edges) => {
   let graph = {};
   for (let edge of edges) {
+    // iterating through every edge
     let [a, b] = edge;
-    if (!(a in graph)) graph[a] = [];
+    if (!(a in graph)) graph[a] = []; // create for the first time
     if (!(b in graph)) graph[b] = [];
     graph[a].push(b);
     graph[b].push(a);
