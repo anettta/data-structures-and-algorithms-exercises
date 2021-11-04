a52c4aca95dab6b551cc6b2cba2f511ef19eac47
Anya <annagapyuk@gmail.com>
2021-11-04 13:59:28 -0400

    added island-count

--- V - Graph/043-island-count.js
+++ V - Graph/043-island-count.js
@@ -6,6 +6,37 @@
 // An island is a vertically or horizontally
 // connected region of land.
 
+const islandCount = (grid) => {
+  // nested loops
+  // depth first w/ visited
+  const visited = new Set();
+  let count = 0;
+  for (let row = 0; row < grid.length; row++) {
+    for (let column = 0; column < grid[0].length; column++) {
+      if (explore(grid, row, column, visited) === true) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
+
+const explore = (grid, row, column, visited) => {
+  const rowInBounds = 0 <= row && row < grid.length;
+  const columnInBounds = 0 <= column && column < grid.length;
+  if (!rowInBounds || !columnInBounds) return false;
+  if (grid[row][column] === "W") return false;
+  const pos = row + "," + column;
+  if (visited.has(pos)) return false;
+  visited.add(pos);
+
+  explore(grid, row - 1, column, visited);
+  explore(grid, row + 1, column, visited);
+  explore(grid, row, column - 1, visited);
+  explore(grid, row, column + 1, visited);
+  return true;
+};
+
 const grid = [
   ["W", "L", "W", "W", "W"],
   ["W", "L", "W", "W", "W"],
