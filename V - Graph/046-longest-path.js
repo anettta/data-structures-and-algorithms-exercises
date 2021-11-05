b4c388412a0113b09273afb025354032372d6f43
Anya <annagapyuk@gmail.com>
2021-11-05 16:47:42 -0400

    added longest-path

--- V - Graph/046-longest-path.js
+++ V - Graph/046-longest-path.js
@@ -8,6 +8,32 @@
 // the number of edges in the path,
 // not the number of nodes.
 
+const longestPath = (graph) => {
+  const distance = {};
+  for (let node in graph) {
+    if (graph[node].length === 0) {
+      distance[node] = 0; // 0 edges away from themselves
+    }
+  }
+  for (let node in graph) {
+    traverseDistance(graph, node, distance);
+  }
+  return Math.max(...Object.values(distance));
+};
+
+const traverseDistance = (graph, node, distance) => {
+  if (node in distance) return distance[node];
+  let max = 0;
+  for (let neighbor of graph[node]) {
+    let attemptLength = traverseDistance(graph, neighbor, distance);
+    if (attemptLength > max) {
+      max = attemptLength;
+    }
+  }
+  distance[node] = 1 + max;
+  return distance[node];
+};
+
 const graph = {
   a: ["c", "b"],
   b: ["c"],
