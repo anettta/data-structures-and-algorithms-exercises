c47f41dd1e4e552fc89b4a896675ddd726b0f77b
Anya <annagapyuk@gmail.com>
2021-11-18 13:14:25 -0500

    added count-paths

---
+++ IV - Dynamic Programming/055-count-paths.js
@@ -0,0 +1,30 @@
+// Write a function, countPaths, that takes in a grid as an argument.
+// In the grid, 'X' represents walls and 'O' represents open spaces.
+// You may only move down or to the right
+// and cannot pass through walls.
+// The function should return the number of ways possible to travel
+// from the top-left corner of the grid to the bottom-right corner.
+//
+// Time: O(r*c) // rows * columns
+// Space: O(r*c) // rows * columns
+//
+const countPaths = (grid, r = 0, c = 0, memo = {}) => {
+  const pos = r + "," + c;
+  if (pos in memo) {
+    return memo[pos];
+  }
+  if (r === grid.length || c === grid[0].length || grid[r][c] === "X") {
+    return 0;
+  }
+  if (r === grid.length - 1 && c === grid[0].length - 1) {
+    return 1;
+  }
+  memo[pos] =
+    countPaths(grid, r + 1, c, memo) + countPaths(grid, r, c + 1, memo);
+  return memo[pos];
+};
+const grid = [
+  ["O", "O"],
+  ["O", "O"],
+];
+console.log(countPaths(grid)); // -> 2
