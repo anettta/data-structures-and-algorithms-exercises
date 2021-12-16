524bc5bc553d1c83e9b3a32f391b2a75653a174c
Anya <annagapyuk@gmail.com>
2021-12-16 13:55:50 -0500

    reviewed 001

--- I - Introduction/001-max-value.js
+++ I - Introduction/001-max-value.js
@@ -1,21 +1,32 @@
-// Write a function `maxValue`, that takes in array of numbers
-// as an argument. The function should return the largest
-// number in the array.
+// Write a function `maxValue`, that takes in array of numbers as an argument.
+// The function should return the largest number in the array.
 //
 // Solve this without using any built-in array methods.
 //
 // You can assume that the array is non-empty
 
+// 12-16-21
+
 const maxValue = (nums) => {
   let largest = -Infinity;
   for (let i = 0; i < nums.length; i++) {
-    let num = nums[i];
-    if (num > largest) {
-      largest = num;
+    if (nums[i] > largest) {
+      largest = nums[i];
     }
   }
   return largest;
 };
+
+// const maxValue = (nums) => {
+//   let largest = -Infinity;
+//   for (let i = 0; i < nums.length; i++) {
+//     let num = nums[i];
+//     if (num > largest) {
+//       largest = num;
+//     }
+//   }
+//   return largest;
+// };
 
 console.log(maxValue([5, 7, 8, 9])); // 9
 console.log(maxValue([-1, -89, -22, -7])); // -1
