3d94281b92eb7a0d215acd1f330bdcc4b1e1bc76
Anya <annagapyuk@gmail.com>
2021-12-16 19:27:13 -0500

    reviewed 004

--- II - Array and String/004-compress.js
+++ II - Array and String/004-compress.js
@@ -9,25 +9,52 @@
 // Space: O(n)
 // Strings in JS are immutable thus strings are copied over
 // using array instead
+
+// 12-16-21
 const compress = (str) => {
   let compressed = [];
-  let i = 0;
-  let j = 0;
+  //"cccatdd"
+  // 3cat2d
+  let i = 0; // start of the streak
+  let j = 0; // end of the streak
+
   while (j <= str.length) {
-    // O(n)
-    if (str[j] === str[i]) {
+    if (str[i] === str[j]) {
       j++;
     } else {
-      let num = j - i;
-      if (num === 1) {
+      let count = j - i;
+      if (count === 1) {
         compressed.push(str[i]);
       } else {
-        compressed.push(num, str[i]);
+        compressed.push(count);
+        compressed.push(str[i]);
       }
       i = j;
     }
   }
-  return compressed.join(""); // O(n)
+
+  return compressed.join("");
 };
+
+// const compress = (str) => {
+//   let compressed = [];
+//   let i = 0;
+//   let j = 0;
+//   while (j <= str.length) {
+//     // O(n)
+//     if (str[j] === str[i]) {
+//       j++;
+//     } else {
+//       let num = j - i;
+//       if (num === 1) {
+//         compressed.push(str[i]);
+//       } else {
+//         compressed.push(num, str[i]);
+//       }
+//       i = j;
+//     }
+//   }
+//   return compressed.join(""); // O(n)
+// };
 
 console.log(compress("ccaaatsss")); // '2c3a1t3s'
