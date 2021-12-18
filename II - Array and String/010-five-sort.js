1c916e4884f45ae61d748ce2902a9cea81387942
Anya <annagapyuk@gmail.com>
2021-12-18 11:54:29 -0500

    reviewed 010

--- II - Array and String/010-five-sort.js
+++ II - Array and String/010-five-sort.js
@@ -9,24 +9,43 @@
 // any order in the output, as long as
 // all 5s are at the end of the array.
 
-const fiveSort = (numbers) => {
-  let pointer1 = 0;
-  let pointer2 = numbers.length - 1;
-  while (pointer1 <= pointer2) {
-    if (numbers[pointer2] === 5) {
-      pointer2--;
-    } else if (numbers[pointer1] === 5) {
-      [numbers[pointer1], numbers[pointer2]] = [
-        numbers[pointer2],
-        numbers[pointer1],
-      ];
-      pointer1++;
+// 12-18-21
+
+const fiveSort = (nums) => {
+  let front = 0;
+  let back = nums.length - 1;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[front] !== 5) {
+      front++;
+    } else if (nums[back] === 5) {
+      back--;
     } else {
-      pointer1++;
+      [nums[front], nums[back]] = [nums[back], nums[front]];
+      front++;
     }
   }
-
-  return numbers;
+  return nums;
 };
 
+// const fiveSort = (numbers) => {
+//   let pointer1 = 0;
+//   let pointer2 = numbers.length - 1;
+//   while (pointer1 <= pointer2) {
+//     if (numbers[pointer2] === 5) {
+//       pointer2--;
+//     } else if (numbers[pointer1] === 5) {
+//       [numbers[pointer1], numbers[pointer2]] = [
+//         numbers[pointer2],
+//         numbers[pointer1],
+//       ];
+//       pointer1++;
+//     } else {
+//       pointer1++;
+//     }
+//   }
+
+//   return numbers;
+// };
+
 console.log(fiveSort([12, 5, 1, 5, 12, 7])); // [12,7,1,12,5,5]
+console.log(fiveSort([5, 5, 5, 5, 12, 7]));
