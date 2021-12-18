5e734775cd6ca8cb615c417482b441fb3209321f
Anya <annagapyuk@gmail.com>
2021-12-17 21:07:12 -0500

    reviewed 007

--- II - Array and String/007-pair-sum.js
+++ II - Array and String/007-pair-sum.js
@@ -8,32 +8,48 @@
 
 // Time: O(n)
 // Space: O(n)
+
 const pairSum = (arr, target) => {
-  // brute force Time: O(n^2) and Space: O(1)
-  //   for (let i = 0; i < arr.length; i++) {
-  //     for (let j = i + 1; j < arr.length; j++) {
-  //       if (arr[i] + arr[j] === target) {
-  //         return [i, j];
-  //       }
-  //     }
-  //     return -1;
-  //   }
-  // Time: O(n)
-  // Space: O(n)
-
   let map = {};
-  for (let i = 0; i < arr.length; i++) {
-    let num = arr[i];
-    let complement = target - num;
 
-    // key in object
+  for (let i = 0; i < arr.length; i++) {
+    let elem = arr[i];
+    // map = [elem, i];
+    let complement = target - elem;
     if (complement in map) {
-      //   console.log(map[complement], complement, i, arr[i]);
       return [map[complement], i];
     }
-    // num's index is i
-    map[num] = i;
+
+    map[elem] = i;
   }
 };
+
+// const pairSum = (arr, target) => {
+//   // brute force Time: O(n^2) and Space: O(1)
+//   //   for (let i = 0; i < arr.length; i++) {
+//   //     for (let j = i + 1; j < arr.length; j++) {
+//   //       if (arr[i] + arr[j] === target) {
+//   //         return [i, j];
+//   //       }
+//   //     }
+//   //     return -1;
+//   //   }
+//   // Time: O(n)
+//   // Space: O(n)
+
+//   let map = {};
+//   for (let i = 0; i < arr.length; i++) {
+//     let num = arr[i];
+//     let complement = target - num;
+
+//     // key in object
+//     if (complement in map) {
+//       //   console.log(map[complement], complement, i, arr[i]);
+//       return [map[complement], i];
+//     }
+//     // num's index is i
+//     map[num] = i;
+//   }
+// };
 
 console.log(pairSum([3, 2, 5, 4, 1], 8)); // [0,2]
