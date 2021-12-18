80275bc63e1ec6c1d19c612b9ee3ea3e30e891f4
Anya <annagapyuk@gmail.com>
2021-12-17 20:50:57 -0500

    reviewed 006

--- II - Array and String/006-most-frequent-char.js
+++ II - Array and String/006-most-frequent-char.js
@@ -5,23 +5,45 @@
 // You can assume that the input string is non-empty.
 // Time: O(n)
 // Space: O(n)
+
+// 12-17-21
+
 const mostFrequentChar = (str) => {
-  let count = {};
-  for (let letter of str) {
-    if (!count[letter]) {
-      count[letter] = 0;
-    }
-    count[letter] += 1; // immediately increment it
-  }
-
   let mostFrequent = null;
-
-  for (let letter of str) {
-    if (mostFrequent === null || count[letter] > count[mostFrequent]) {
-      mostFrequent = letter;
+  let count = {};
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    // catts
+    if (!count[char]) {
+      count[char] = 1;
+    } else {
+      count[char] = count[char] + 1;
+    }
+  }
+  for (let elem of str) {
+    if (mostFrequent === null || count[elem] > count[mostFrequent]) {
+      mostFrequent = elem;
     }
   }
   return mostFrequent;
 };
+// const mostFrequentChar = (str) => {
+//   let count = {};
+//   for (let letter of str) {
+//     if (!count[letter]) {
+//       count[letter] = 0;
+//     }
+//     count[letter] += 1; // immediately increment it
+//   }
+
+//   let mostFrequent = null;
+
+//   for (let letter of str) {
+//     if (mostFrequent === null || count[letter] > count[mostFrequent]) {
+//       mostFrequent = letter;
+//     }
+//   }
+//   return mostFrequent;
+// };
 
 console.log(mostFrequentChar("bookeeper")); // 'e'
