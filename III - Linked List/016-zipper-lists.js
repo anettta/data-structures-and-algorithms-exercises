ca22e770bdbaf7adcf1800e4b4284a73bffe1d22
Anya <annagapyuk@gmail.com>
2021-12-25 21:44:53 -0500

    reviewed 016

--- III - Linked List/016-zipper-lists.js
+++ III - Linked List/016-zipper-lists.js
@@ -14,7 +14,7 @@ class Node {
   }
 }
 
-// 12-21-21
+// 12-25-21
 
 const zipperLists = (head1, head2) => {
   let current1 = head1.next;
@@ -30,20 +30,50 @@ const zipperLists = (head1, head2) => {
       tail.next = current1;
       current1 = current1.next;
     }
-
     tail = tail.next;
     count++;
+  }
+
+  if (current2 === null) {
+    tail.next = current1;
   }
 
   if (current1 === null) {
     tail.next = current2;
   }
-  if (current2 === null) {
-    tail.next = current1;
-  }
-
   return head1;
 };
+
+// 12-21-21
+
+// const zipperLists = (head1, head2) => {
+//   let current1 = head1.next;
+//   let current2 = head2;
+//   let tail = head1;
+//   let count = 0;
+
+//   while (current1 && current2) {
+//     if (count % 2 === 0) {
+//       tail.next = current2;
+//       current2 = current2.next;
+//     } else {
+//       tail.next = current1;
+//       current1 = current1.next;
+//     }
+
+//     tail = tail.next;
+//     count++;
+//   }
+
+//   if (current1 === null) {
+//     tail.next = current2;
+//   }
+//   if (current2 === null) {
+//     tail.next = current1;
+//   }
+
+//   return head1;
+// };
 
 //
 // iterative
