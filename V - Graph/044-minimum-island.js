56dc90a16991947e129b41a7c470d913efe166ef
Anya <annagapyuk@gmail.com>
2022-01-12 12:26:36 -0500

    reviewed 044

--- V - Graph/044-minimum-island.js
+++ V - Graph/044-minimum-island.js
@@ -9,39 +9,76 @@
 // You may assume that the grid contains at least one island.
 
 const minimumIsland = (grid) => {
-  // nested loops
-  // depth first w/ visited
-  const visited = new Set();
   let min = Infinity;
-  for (let row = 0; row < grid.length; row++) {
-    for (let column = 0; column < grid[0].length; column++) {
-      const size = exploreSize(grid, row, column, visited);
-      if (size > 0 && size < min) {
+  let visited = new Set();
+
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[0].length; c++) {
+      let size = explore(grid, r, c, visited);
+      if (size < min && size > 0) {
         min = size;
       }
     }
   }
+
   return min;
 };
 
-const exploreSize = (grid, row, column, visited) => {
-  const rowInBounds = 0 <= row && row < grid.length;
-  const columnInBounds = 0 <= column && column < grid[0].length;
+const explore = (grid, r, c, visited) => {
+  const rowInBounds = 0 <= r && r < grid.length;
+  const columnInBounds = 0 <= c && c < grid[0].length;
   if (!rowInBounds || !columnInBounds) return 0;
 
-  if (grid[row][column] === "W") return 0;
+  if (grid[r][c] === "W") return 0;
 
-  const pos = row + "," + column;
-  if (visited.has(pos)) return 0;
-  visited.add(pos);
+  let position = r + "," + c;
+  if (visited.has(position)) return 0;
+  visited.add(position);
 
-  let size = 1; // current position
-  size += exploreSize(grid, row - 1, column, visited);
-  size += exploreSize(grid, row + 1, column, visited);
-  size += exploreSize(grid, row, column - 1, visited);
-  size += exploreSize(grid, row, column + 1, visited);
+  let size = 1;
+
+  size += explore(grid, r - 1, c, visited);
+  size += explore(grid, r + 1, c, visited);
+  size += explore(grid, r, c - 1, visited);
+  size += explore(grid, r, c + 1, visited);
+
   return size;
 };
+
+// const minimumIsland = (grid) => {
+//   // nested loops
+//   // depth first w/ visited
+//   const visited = new Set();
+//   let min = Infinity;
+//   for (let row = 0; row < grid.length; row++) {
+//     for (let column = 0; column < grid[0].length; column++) {
+//       const size = exploreSize(grid, row, column, visited);
+//       if (size > 0 && size < min) {
+//         min = size;
+//       }
+//     }
+//   }
+//   return min;
+// };
+
+// const exploreSize = (grid, row, column, visited) => {
+//   const rowInBounds = 0 <= row && row < grid.length;
+//   const columnInBounds = 0 <= column && column < grid[0].length;
+//   if (!rowInBounds || !columnInBounds) return 0;
+
+//   if (grid[row][column] === "W") return 0;
+
+//   const pos = row + "," + column;
+//   if (visited.has(pos)) return 0;
+//   visited.add(pos);
+
+//   let size = 1; // current position
+//   size += exploreSize(grid, row - 1, column, visited);
+//   size += exploreSize(grid, row + 1, column, visited);
+//   size += exploreSize(grid, row, column - 1, visited);
+//   size += exploreSize(grid, row, column + 1, visited);
+//   return size;
+// };
 const grid = [
   ["W", "L", "W", "W", "W"],
   ["W", "L", "W", "W", "W"],
