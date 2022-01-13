c0d75919304f4096aa2b3a3a0bf26f79a4ee77ff
Anya <annagapyuk@gmail.com>
2022-01-13 13:22:37 -0500

    reviewed 055

--- IV - Dynamic Programming/055-count-paths.js
+++ IV - Dynamic Programming/055-count-paths.js
@@ -8,21 +8,33 @@
 // Time: O(r*c) // rows * columns
 // Space: O(r*c) // rows * columns
 //
+
 const countPaths = (grid, r = 0, c = 0, memo = {}) => {
   const pos = r + "," + c;
-  if (pos in memo) {
-    return memo[pos];
-  }
-  if (r === grid.length || c === grid[0].length || grid[r][c] === "X") {
-    return 0;
-  }
-  if (r === grid.length - 1 && c === grid[0].length - 1) {
-    return 1;
-  }
-  memo[pos] =
-    countPaths(grid, r + 1, c, memo) + countPaths(grid, r, c + 1, memo);
+  if (pos in memo) return memo[pos];
+  if (r === grid.length || c === grid[0].length || grid[r][c] === "X") return 0;
+  if (r === grid.length - 1 && c === grid[0].length - 1) return 1;
+  const down = countPaths(grid, r + 1, c, memo);
+  const right = countPaths(grid, r, c + 1, memo);
+  memo[pos] = down + right;
   return memo[pos];
 };
+
+// const countPaths = (grid, r = 0, c = 0, memo = {}) => {
+//   const pos = r + "," + c;
+//   if (pos in memo) {
+//     return memo[pos];
+//   }
+//   if (r === grid.length || c === grid[0].length || grid[r][c] === "X") {
+//     return 0;
+//   }
+//   if (r === grid.length - 1 && c === grid[0].length - 1) {
+//     return 1;
+//   }
+//   memo[pos] =
+//     countPaths(grid, r + 1, c, memo) + countPaths(grid, r, c + 1, memo);
+//   return memo[pos];
+// };
 const grid = [
   ["O", "O"],
   ["O", "O"],
