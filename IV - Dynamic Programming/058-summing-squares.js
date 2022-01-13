0b39b014df7e30c0ffd4dd4f8b2de61262c3d30f
Anya <annagapyuk@gmail.com>
2022-01-13 16:41:27 -0500

    reviewed 058

--- IV - Dynamic Programming/058-summing-squares.js
+++ IV - Dynamic Programming/058-summing-squares.js
@@ -1,9 +1,10 @@
-// Write a function, summingSquares, that takes a target number as an argument. The function should return the minimum number of perfect squares that sum to the target. A perfect square is a number of the form (i*i) where i >= 1.
+// Write a function, summingSquares, that takes a target number as an argument.
+// The function should return the minimum number of perfect squares that sum to the target.
+// A perfect square is a number of the form (i*i) where i >= 1.
 
 // For example: 1, 4, 9, 16 are perfect squares, but 8 is not perfect square.
 
 // Given 12:
-
 // summingSquares(12) -> 3
 
 // The minimum squares required for 12 is three, by doing 4 + 4 + 4.
@@ -13,18 +14,32 @@
 // Time: O(n * sqrt(n))
 // Space: O(n)
 //
-const summingSquares = (n, memo = {}) => {
-  if (n in memo) return memo[n];
-  if (n === 0) return 0;
 
+const summingSquares = (num, memo = {}) => {
+  if (num in memo) return memo[num];
+  if (num === 0) return 0;
   let minSquares = Infinity;
-  for (let i = 1; i <= Math.sqrt(n); i++) {
+  for (let i = 1; i <= Math.sqrt(num); i++) {
     const square = i * i;
-    const numSquares = 1 + summingSquares(n - square, memo);
-    minSquares = Math.min(minSquares, numSquares);
+    const numSquares = 1 + summingSquares(num - square, memo);
+    minSquares = Math.min(numSquares, minSquares);
   }
-  memo[n] = minSquares;
+  memo[num] = minSquares;
   return minSquares;
 };
+
+// const summingSquares = (n, memo = {}) => {
+//   if (n in memo) return memo[n];
+//   if (n === 0) return 0;
+
+//   let minSquares = Infinity;
+//   for (let i = 1; i <= Math.sqrt(n); i++) {
+//     const square = i * i;
+//     const numSquares = 1 + summingSquares(n - square, memo);
+//     minSquares = Math.min(minSquares, numSquares);
+//   }
+//   memo[n] = minSquares;
+//   return minSquares;
+// };
 
 console.log(summingSquares(8)); // -> 2
