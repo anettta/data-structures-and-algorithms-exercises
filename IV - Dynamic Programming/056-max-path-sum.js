4f8355824067b7562cbd7c921d4f350a1b086168
Anya <annagapyuk@gmail.com>
2022-01-13 14:05:01 -0500

    reviewed 056

--- IV - Dynamic Programming/056-max-path-sum.js
+++ IV - Dynamic Programming/056-max-path-sum.js
@@ -1,4 +1,7 @@
-// Write a function, maxPathSum, that takes in a grid as an argument. The function should return the maximum sum possible by traveling a path from the top-left corner to the bottom-right corner. You may only travel through the grid by moving down or right.
+// Write a function, maxPathSum, that takes in a grid as an argument.
+// The function should return the maximum sum possible
+// by traveling a path from the top-left corner to the bottom-right corner.
+// You may only travel through the grid by moving down or right.
 
 // You can assume that all numbers are non-negative.
 //
@@ -6,17 +9,30 @@
 // Space: O(r*c)
 //
 //
+
 const maxPathSum = (grid, r = 0, c = 0, memo = {}) => {
   const pos = r + "," + c;
   if (pos in memo) return memo[pos];
   if (r === grid.length || c === grid[0].length) return -Infinity;
   if (r === grid.length - 1 && c === grid[0].length - 1) return grid[r][c];
 
-  const down = maxPathSum(grid, r + 1, c, memo);
-  const right = maxPathSum(grid, r, c + 1, memo);
-  memo[pos] = grid[r][c] + Math.max(down, right);
+  let maxDownPath = maxPathSum(grid, r + 1, c, memo);
+  let maxRightPath = maxPathSum(grid, r, c + 1, memo);
+  memo[pos] = grid[r][c] + Math.max(maxDownPath, maxRightPath);
   return memo[pos];
 };
+
+// const maxPathSum = (grid, r = 0, c = 0, memo = {}) => {
+//   const pos = r + "," + c;
+//   if (pos in memo) return memo[pos];
+//   if (r === grid.length || c === grid[0].length) return -Infinity;
+//   if (r === grid.length - 1 && c === grid[0].length - 1) return grid[r][c];
+
+//   const down = maxPathSum(grid, r + 1, c, memo);
+//   const right = maxPathSum(grid, r, c + 1, memo);
+//   memo[pos] = grid[r][c] + Math.max(down, right);
+//   return memo[pos];
+// };
 
 const grid = [
   [1, 3, 12],
