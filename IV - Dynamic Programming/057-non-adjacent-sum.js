62eb20a86b1ff86d587abe29c6c574741cdc145c
Anya <annagapyuk@gmail.com>
2022-01-13 16:22:35 -0500

    reviewed 057

--- IV - Dynamic Programming/057-non-adjacent-sum.js
+++ IV - Dynamic Programming/057-non-adjacent-sum.js
@@ -1,4 +1,6 @@
-// Write a function, nonAdjacentSum, that takes in an array of numbers as an argument. The function should return the maximum sum of non-adjacent elements in the array. There is no limit on how many elements can be taken into the sum as long as they are not adjacent.
+// Write a function, nonAdjacentSum, that takes in an array of numbers as an argument.
+// The function should return the maximum sum of non-adjacent elements in the array.
+// There is no limit on how many elements can be taken into the sum as long as they are not adjacent.
 
 // For example, given:
 
@@ -12,14 +14,25 @@
 // Space: O(n)
 //
 //
+
 const nonAdjacentSum = (nums, i = 0, memo = {}) => {
   if (i in memo) return memo[i];
   if (i >= nums.length) return 0;
+
   const include = nums[i] + nonAdjacentSum(nums, i + 2, memo);
-  const exclude = nonAdjacentSum(nums, i + 1, memo);
-  memo[i] = Math.max(include, exclude);
+  const exlude = nonAdjacentSum(nums, i + 1, memo);
+  memo[i] = Math.max(include, exlude);
   return memo[i];
 };
+
+// const nonAdjacentSum = (nums, i = 0, memo = {}) => {
+//   if (i in memo) return memo[i];
+//   if (i >= nums.length) return 0;
+//   const include = nums[i] + nonAdjacentSum(nums, i + 2, memo);
+//   const exclude = nonAdjacentSum(nums, i + 1, memo);
+//   memo[i] = Math.max(include, exclude);
+//   return memo[i];
+// };
 
 const nums = [2, 4, 5, 12, 7];
 console.log(nonAdjacentSum(nums)); // -> 16
