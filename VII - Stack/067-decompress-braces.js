f6f466f6a5e8b42d9228a7551a14409a815de04d
Anya <annagapyuk@gmail.com>
2022-01-14 18:26:52 -0500

    added newproblem

---
+++ VII - Stack/067-decompress-braces.js
@@ -0,0 +1,32 @@
+const decompressBraces = (str) => {
+  let stack = [];
+  const numberChars = "0123456789";
+
+  for (let char of str) {
+    if (numberChars.includes(char)) {
+      stack.push(Number(char));
+    } else {
+      if (char === "}") {
+        let segment = "";
+        while (typeof stack[stack.length - 1] !== "number") {
+          const popped = stack.pop();
+          segment = popped + segment;
+        }
+        const num = stack.pop();
+        stack.push(repeat(segment, num));
+      } else if (char !== "{") {
+        stack.push(char);
+      }
+    }
+  }
+  return stack.join("");
+};
+
+const repeat = (elem, num) => {
+  let result = "";
+  for (let i = 0; i < num; i++) {
+    result += elem;
+  }
+  return result;
+};
+console.log(decompressBraces("3r9{u}"));
