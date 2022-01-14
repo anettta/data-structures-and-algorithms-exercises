17a6f2bda2153610be2174f33c7afadc89480d57
Anya <annagapyuk@gmail.com>
2022-01-14 14:54:50 -0500

    solved paired parentheses problem

--- VII - Stack/065-paired-parentheses.js
+++ VII - Stack/065-paired-parentheses.js
@@ -1,1 +1,19 @@
-// first stack exercise
+// write a function, pairedParentheses, that takes in
+// a string as an argument.
+// the function should return a boolean indicating whether
+// or not the string has well-formed parentheses.
+
+const pairedParentheses = (str) => {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "(") {
+      count++;
+    } else {
+      count--;
+    }
+  }
+  return count === 0 ? true : false;
+};
+
+console.log(pairedParentheses("(())"));
+console.log(pairedParentheses("((**)"));
