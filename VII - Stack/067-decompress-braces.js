29b451b35c0de3679322be030ed0887f93a9f270
Anya <annagapyuk@gmail.com>
2022-01-14 21:05:17 -0500

    reviewed 067

--- VII - Stack/067-decompress-braces.js
+++ VII - Stack/067-decompress-braces.js
@@ -1,19 +1,18 @@
 const decompressBraces = (str) => {
   let stack = [];
-  const numberChars = "0123456789";
-
+  let numbers = "0123456789";
   for (let char of str) {
-    if (numberChars.includes(char)) {
+    if (numbers.includes(char)) {
       stack.push(Number(char));
     } else {
       if (char === "}") {
         let segment = "";
         while (typeof stack[stack.length - 1] !== "number") {
-          const popped = stack.pop();
+          let popped = stack.pop();
           segment = popped + segment;
         }
-        const num = stack.pop();
-        stack.push(repeat(segment, num));
+        let num = stack.pop();
+        stack.push(repeat(num, segment));
       } else if (char !== "{") {
         stack.push(char);
       }
@@ -22,11 +21,43 @@ const decompressBraces = (str) => {
   return stack.join("");
 };
 
-const repeat = (elem, num) => {
+const repeat = (num, char) => {
   let result = "";
   for (let i = 0; i < num; i++) {
-    result += elem;
+    result += char;
   }
   return result;
 };
+
+// const decompressBraces = (str) => {
+//   let stack = [];
+//   const numberChars = "0123456789";
+
+//   for (let char of str) {
+//     if (numberChars.includes(char)) {
+//       stack.push(Number(char));
+//     } else {
+//       if (char === "}") {
+//         let segment = "";
+//         while (typeof stack[stack.length - 1] !== "number") {
+//           const popped = stack.pop();
+//           segment = popped + segment;
+//         }
+//         const num = stack.pop();
+//         stack.push(repeat(segment, num));
+//       } else if (char !== "{") {
+//         stack.push(char);
+//       }
+//     }
+//   }
+//   return stack.join("");
+// };
+
+// const repeat = (elem, num) => {
+//   let result = "";
+//   for (let i = 0; i < num; i++) {
+//     result += elem;
+//   }
+//   return result;
+// };
 console.log(decompressBraces("3r9{u}"));
