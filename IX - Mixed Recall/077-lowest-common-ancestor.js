4bc07d986fd1a464faf74cc724e34e7650b0fbd1
Anya <annagapyuk@gmail.com>
2022-01-17 16:48:14 -0500

    reviewed 077

--- IX - Mixed Recall/077-lowest-common-ancestor.js
+++ IX - Mixed Recall/077-lowest-common-ancestor.js
@@ -7,11 +7,13 @@ class Node {
 }
 
 const lowestCommonAncestor = (root, val1, val2) => {
-  const path1 = getPath(root, val1);
-  const path2 = getPath(root, val2);
-  const set2 = new Set(path2);
+  let path1 = getPath(root, val1);
+  let path2 = getPath(root, val2);
+  let set1 = new Set(path2);
   for (let val of path1) {
-    if (set2.has(val)) return val;
+    if (set1.has(val)) {
+      return val;
+    }
   }
 };
 
@@ -29,9 +31,35 @@ const getPath = (root, target) => {
     rightPath.push(root.val);
     return rightPath;
   }
-
   return null;
 };
+
+// const lowestCommonAncestor = (root, val1, val2) => {
+//   const path1 = getPath(root, val1);
+//   const path2 = getPath(root, val2);
+//   const set2 = new Set(path2);
+//   for (let val of path1) {
+//     if (set2.has(val)) return val;
+//   }
+// };
+
+// const getPath = (root, target) => {
+//   if (root === null) return null;
+//   if (root.val === target) return [root.val];
+
+//   let leftPath = getPath(root.left, target);
+//   if (leftPath !== null) {
+//     leftPath.push(root.val);
+//     return leftPath;
+//   }
+//   let rightPath = getPath(root.right, target);
+//   if (rightPath !== null) {
+//     rightPath.push(root.val);
+//     return rightPath;
+//   }
+
+//   return null;
+// };
 
 const a = new Node("a");
 const b = new Node("b");
