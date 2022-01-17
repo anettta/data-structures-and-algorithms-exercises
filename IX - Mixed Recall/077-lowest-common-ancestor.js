c3c001e75bfd60161b366214f36876b4cb26d2d2
Anya <annagapyuk@gmail.com>
2022-01-17 16:24:33 -0500

    added 077

---
+++ IX - Mixed Recall/077-lowest-common-ancestor.js
@@ -0,0 +1,49 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const lowestCommonAncestor = (root, val1, val2) => {
+  const path1 = getPath(root, val1);
+  const path2 = getPath(root, val2);
+  const set2 = new Set(path2);
+  for (let val of path1) {
+    if (set2.has(val)) return val;
+  }
+};
+
+const getPath = (root, target) => {
+  if (root === null) return null;
+  if (root.val === target) return [root.val];
+
+  let leftPath = getPath(root.left, target);
+  if (leftPath !== null) {
+    leftPath.push(root.val);
+    return leftPath;
+  }
+  let rightPath = getPath(root.right, target);
+  if (rightPath !== null) {
+    rightPath.push(root.val);
+    return rightPath;
+  }
+
+  return null;
+};
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+
+a.left = b;
+a.right = c;
+c.left = d;
+c.right = e;
+//               a
+//            b      c
+//                d     e
+console.log(lowestCommonAncestor(a, "d", "e"));
