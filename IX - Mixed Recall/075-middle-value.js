281cb1d616729bcaa6460f3405dc003c8bb67c92
Anya <annagapyuk@gmail.com>
2022-01-17 14:24:07 -0500

    added 075

---
+++ IX - Mixed Recall/075-middle-value.js
@@ -0,0 +1,43 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+const middleValue = (head) => {
+  //Time linear, space - linear
+  //
+  //   let current = head;
+  //   let values = [];
+  //   while (current) {
+  //     values.push(current.val);
+  //     current = current.next;
+  //   }
+  //   let mid = Math.floor(values.length / 2);
+
+  //   return values[mid];
+  //
+  //
+  //
+  // slow and fast pointers Time linear, space - constant
+  //
+  let slow = head;
+  let fast = head;
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow.val;
+};
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+console.log(middleValue(a));
