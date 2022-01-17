5f55a67683ad76d7269d7eb4b1179ae5e054388b
Anya <annagapyuk@gmail.com>
2022-01-17 16:55:51 -0500

    added 078

---
+++ IX - Mixed Recall/078-flip-tree.js
@@ -0,0 +1,32 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const flipTree = (root) => {
+  if (root === null) return null;
+  const right = flipTree(root.right);
+  const left = flipTree(root.left);
+  root.left = right;
+  root.right = left;
+
+  return root;
+};
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+
+a.left = b;
+a.right = c;
+c.left = d;
+c.right = e;
+//               a
+//            b      c
+//                d     e
+console.log(flipTree(a));
