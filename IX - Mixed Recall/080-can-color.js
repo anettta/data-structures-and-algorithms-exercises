a5d69ca55b7bb428c5c35f3b3611c57c55f7e9f9
Anya <annagapyuk@gmail.com>
2022-01-17 18:50:22 -0500

    added 080

---
+++ IX - Mixed Recall/080-can-color.js
@@ -0,0 +1,35 @@
+const canColor = (graph) => {
+  const coloring = {};
+
+  for (let node in graph) {
+    if (!(node in coloring)) {
+      if (!validate(graph, node, coloring, false)) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
+
+const validate = (graph, node, coloring, currentColor) => {
+  if (node in coloring) return coloring[node] === currentColor;
+  coloring[node] = currentColor;
+  for (let neighbor in graph[node]) {
+    if (!validate(graph, neighbor, coloring, !currentColor)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const graph = {
+  a: ["b"],
+  b: ["c", "e"],
+  c: ["e"],
+  e: ["b"],
+};
+
+// a -> b -> c -> e
+
+console.log(canColor(graph));
