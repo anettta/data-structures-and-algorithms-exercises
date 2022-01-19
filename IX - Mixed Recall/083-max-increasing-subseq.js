52e07b3997c4f6809c5a3c28d412c95e2a262973
Anya <annagapyuk@gmail.com>
2022-01-19 17:24:49 -0500

    practiced with dynamic programming-083

---
+++ IX - Mixed Recall/083-max-increasing-subseq.js
@@ -0,0 +1,22 @@
+const maxIncreasingSubseq = (nums, i = 0, prev = -Infinity, memo = {}) => {
+  const key = i + "," + prev;
+  if (key in memo) return memo[key];
+  if (i === nums.length) {
+    return 0;
+  }
+
+  let currentNum = nums[i];
+  let options = [];
+  let withoutCurrentNum = maxIncreasingSubseq(nums, i + 1, prev, memo);
+
+  options.push(withoutCurrentNum);
+
+  if (currentNum > prev) {
+    let withCurrentNum = 1 + maxIncreasingSubseq(nums, i + 1, currentNum, memo);
+    options.push(withCurrentNum);
+  }
+  memo[key] = Math.max(...options);
+  return memo[key];
+};
+
+console.log(maxIncreasingSubseq([2, 4, 9, 5]));
