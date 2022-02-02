be1c2c7baf6c67d605856fca043249b428c4833e
Anya <annagapyuk@gmail.com>
2022-02-02 13:15:25 -0500

    added memo object to improve time complexity

--- VI - Dynamic Programming/064-quickestConcat.js
+++ VI - Dynamic Programming/064-quickestConcat.js
@@ -3,20 +3,21 @@ const quickConcat = (s, words) => {
   return result === Infinity ? -1 : result;
 };
 
-const quickestConcat = (s, words) => {
+const quickestConcat = (s, words, memo = {}) => {
   if (s === "") return 0;
-
+  if (s in memo) return memo[s];
   let min = Infinity;
 
   for (let word of words) {
     if (s.startsWith(word) === true) {
       const suffix = s.slice(word.length);
-      const currentSize = 1 + quickestConcat(suffix, words);
+      const currentSize = 1 + quickestConcat(suffix, words, memo);
       if (currentSize < min) {
         min = currentSize;
       }
     }
   }
+  memo[s] = min;
   return min;
 };
 
