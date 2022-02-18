d4bdf71ce605b126bd2049bd45161d93aed6d1b3
Anya <annagapyuk@gmail.com>
2022-02-17 22:04:10 -0500

    added binary search

---
+++ IX - Mixed Recall/088-binary-search.js
@@ -0,0 +1,17 @@
+const binarySearch = (nums, target) => {
+  let lo = 0;
+  let hi = nums.length - 1;
+  while (lo <= hi) {
+    const mid = Math.floor((lo + hi) / 2);
+    if (nums[mid] < target) {
+      lo = mid + 1;
+    } else if (nums[mid] > target) {
+      hi = mid - 1;
+    } else {
+      return mid;
+    }
+  }
+  return -1;
+};
+
+console.log(binarySearch([2, 3, 6, 9], 6)); // 2
