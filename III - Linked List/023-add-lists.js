16134633b6686d7aa5e2b16eb2de317332e5e25f
Anya <annagapyuk@gmail.com>
2022-05-09 18:13:34 -0400

    reviewwed addList

--- III - Linked List/023-add-lists.js
+++ III - Linked List/023-add-lists.js
@@ -30,9 +30,9 @@ class Node {
 }
 
 const addLists = (head1, head2) => {
+  let carry = 0;
   let current1 = head1;
   let current2 = head2;
-  let carry = 0;
   let dummyHead = new Node(null);
   let tail = dummyHead;
 
@@ -40,8 +40,9 @@ const addLists = (head1, head2) => {
     let val1 = current1 === null ? 0 : current1.val;
     let val2 = current2 === null ? 0 : current2.val;
     let sum = val1 + val2 + carry;
-    carry = sum > 9 ? 1 : 0;
     let digit = sum % 10;
+    carry = sum > 9 ? 1 : 0;
+
     if (current1 !== null) {
       current1 = current1.next;
     }
@@ -49,116 +50,28 @@ const addLists = (head1, head2) => {
       current2 = current2.next;
     }
     let newNode = new Node(digit);
+
     tail.next = newNode;
     tail = tail.next;
   }
-
   return dummyHead.next;
 };
 
 // const addLists = (head1, head2, carry = 0) => {
-//   if (head1 === null && head2 === null && carry === 0) return null;
+//   if (head1 === null && head2 === null && carry === 0) {
+//     return null;
+//   }
 //   let val1 = head1 === null ? 0 : head1.val;
 //   let val2 = head2 === null ? 0 : head2.val;
 //   let sum = val1 + val2 + carry;
-//   let nextCarry = sum > 9 ? 1 : 0;
 //   let digit = sum % 10;
-//   let newNode = new Node(digit);
+//   let nextCarry = sum > 9 ? 1 : 0;
 
 //   let next1 = head1 === null ? null : head1.next;
 //   let next2 = head2 === null ? null : head2.next;
+//   let newNode = new Node(digit);
 //   newNode.next = addLists(next1, next2, nextCarry);
 //   return newNode;
-// };
-
-// const addLists = (head1, head2) => {
-//   let carry = 0;
-//   let current1 = head1;
-//   let current2 = head2;
-//   let dummyHead = new Node(null);
-//   let tail = dummyHead;
-
-//   while (current1 !== null || current2 !== null || carry === 1) {
-//     const val1 = current1 === null ? 0 : current1.val;
-//     const val2 = current2 === null ? 0 : current2.val;
-//     let sum = carry + val1 + val2;
-
-//     let digit = sum % 10;
-//     carry = sum > 9 ? 1 : 0;
-
-//     if (current1 !== null) {
-//       current1 = current1.next;
-//     }
-//     if (current2 !== null) {
-//       current2 = current2.next;
-//     }
-
-//     tail.next = new Node(digit);
-//     tail = tail.next;
-//   }
-//   return dummyHead.next;
-// };
-
-// const addLists = (head1, head2, carry = 0) => {
-//   if (head1 === null && head2 === null && carry === 0) return null;
-
-//   const val1 = head1 === null ? 0 : head1.val;
-//   const val2 = head2 === null ? 0 : head2.val;
-
-//   let sum = carry + val1 + val2;
-
-//   let nextCarry = sum > 9 ? 1 : 0;
-//   const digit = sum % 10;
-//   let result = new Node(digit);
-//   const next1 = head1 === null ? null : head1.next;
-//   const next2 = head2 === null ? null : head2.next;
-//   result.next = addLists(next1, next2, nextCarry);
-//   return result;
-// };
-
-// recursive
-// const addLists = (head1, head2, carry = 0) => {
-//   if (head1 === null && head2 === null && carry === 0) return null;
-
-//   const val1 = head1 === null ? 0 : head1.val;
-//   const val2 = head2 === null ? 0 : head2.val;
-
-//   let sum = val1 + val2 + carry;
-
-//   const nextCarry = sum > 9 ? 1 : 0;
-//   const digit = sum % 10;
-//   let resultNode = new Node(digit);
-
-//   const next1 = head1 === null ? null : head1.next;
-//   const next2 = head2 === null ? null : head2.next;
-
-//   resultNode.next = addLists(next1, next2, nextCarry);
-//   return resultNode;
-// };
-
-// const addLists = (head1, head2) => {
-//   let carry = 0;
-//   let dummy = new Node(null);
-//   let tail = dummy;
-
-//   while (head1 !== null || head2 !== null || carry === 1) {
-//     const val1 = head1 === null ? 0 : head1.val;
-//     const val2 = head2 === null ? 0 : head2.val;
-//     const sum = val1 + val2 + carry;
-
-//     carry = sum > 9 ? 1 : 0;
-//     const digit = sum % 10;
-
-//     let newNode = new Node(digit);
-
-//     if (head1 !== null) head1 = head1.next;
-//     if (head2 !== null) head2 = head2.next;
-
-//     tail.next = newNode;
-//     tail = tail.next;
-//   }
-
-//   return dummy.next;
 // };
 
 const a1 = new Node(9);
